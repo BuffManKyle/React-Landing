7d703a5cdf0b001a971b5c98f6c266596085e81f
BuffManKyle <epsolfwd@gmail.com>
2023-04-22 21:42:51 -0400

    First Commit, trying to see mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './assets/Resolve_Barbell_Logo_transparent_notext.png';
+import bird from './assets/Bird_Transparent.jpg.png';
 import './App.css';
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='Logo-text'>Resolve Barbell</h1>
+        <img src={bird} className="bird" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
