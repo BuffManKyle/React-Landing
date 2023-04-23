7a2486317f5884fe321db2583bca16037c6b2a6e
BuffManKyle <epsolfwd@gmail.com>
2023-04-23 17:02:36 -0400

    Switched to grids for layout

--- src/App.js
+++ src/App.js
@@ -5,22 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className='Logo-text'>Resolve Barbell</h1>
         <img src={bird} className="bird" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='statment'>
+        Annapolis, Maryland is about to welcome a brand new gym called Resolve Barbell to its fitness scene. This state-of-the-art gym is set to offer the latest fitness equipment and facilities to its members, ranging from weightlifting and powerlifting equipment to cardio machines and group fitness classes.
+
+        The gym's owner is passionate about fitness and aim to create a welcoming and supportive environment for gym-goers of all levels, from beginners to experienced athletes. In addition to the top-notch equipment, Resolve Barbell will also offer personalized training and coaching services to help members achieve their fitness goals.
+
+        The gym's location in Annapolis is ideal for those looking to get fit in a convenient and accessible location. With its grand opening just around the corner, residents of Annapolis and the surrounding areas can look forward to a new and exciting addition to their fitness options.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
